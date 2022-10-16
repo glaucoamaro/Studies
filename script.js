91f7206fd9d464a6873d1c2955ad7a4cf0bf52fe
Glauco Amaro <115508472+glaucoamaro@users.noreply.github.com>
2022-10-16 15:31:14 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('#button')
-
-button.addEventListener('click', () => (
-    window.scroll({top: window.innerHeight, behavior: 'smooth'}), 
-    button.style.display = 'none'
-))
